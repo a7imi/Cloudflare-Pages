04425a231c323de0dc135412145cf52d9b86feb0
a7imi <128510758+a7imi@users.noreply.github.com>
2024-08-06 00:32:06 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'ns.moghavemat.site'
+      url.hostname = 'u8i.pages.dev'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
