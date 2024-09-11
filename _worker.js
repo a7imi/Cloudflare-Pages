6e488446579ce815844cc70b83f73e14000cd9e6
a7imi <128510758+a7imi@users.noreply.github.com>
2024-09-11 16:54:24 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'ns0.digikala.pl'
+      url.hostname = 'ns10.bnmod.me'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
